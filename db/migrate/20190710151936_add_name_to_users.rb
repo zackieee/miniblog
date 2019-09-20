4584b7b97e463eaae942c3351d81e5018465a322
zackieee <zackie.jackie.222@gmail.com>
2019-09-21 01:35:04 +0900

    update db migration file

--- db/migrate/20190710151936_add_name_to_users.rb
+++ db/migrate/20190710151936_add_name_to_users.rb
@@ -1,5 +1,5 @@
 class AddNameToUsers < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :users, :name, :string
   end
 end
--- db/migrate/20190710152921_add_userid_to_blogs.rb
+++ db/migrate/20190710152921_add_userid_to_blogs.rb
@@ -1,5 +1,5 @@
 class AddUseridToBlogs < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :blogs, :user_id, :integer, null:false
   end
 end
