0f525141953a7ed2f5872e2267b0b21730400c6c
zackieee <zackie.jackie.222@gmail.com>
2019-09-21 17:51:41 +0900

    バグ対応

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
     super do                                             # 他はdeviseの機能をそのまま流用する
-      resource.skip_confirmation!
+      # resource.skip_confirmation!
       # resource.save
-      # resource.update(confirmed_at: Time .now.utc)       # skip
+      resource.update(confirmed_at: Time .now.utc)       # skip
     end
   end
 end
