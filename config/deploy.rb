8bbf6335b9cd550f62937d4fa1bd6a8de523ad8c
zackieee <zackie.jackie.222@gmail.com>
2019-09-21 16:04:19 +0900

    設定ファイル誤記修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,13 +6,13 @@ lock '3.11.0'
 set :application, 'miniblog'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:zackieeee/miniblog.git'
+set :repo_url,  'git@github.com:zackieee/miniblog.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '<このアプリで使用しているrubyのバージョン>' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
