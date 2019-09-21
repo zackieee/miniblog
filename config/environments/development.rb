2d1c12c7eb38d09fa2fd68f13e61a2a897393be9
zackieee <zackie.jackie.222@gmail.com>
2019-09-21 20:25:11 +0900

    メール設定変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,19 +32,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
   # ActionMailer::Base.delivery_method = :letter_opener
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:"smtp.gmail.com",
-  #   domain: 'gmail.com',
-  #   port:587,
-  #   user_name: Rails.application.credentials.gmail[:user_name],
-  #   password: Rails.application.credentials.gmail[:password],
-  #   authentication: :plain,
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: :login
+  }
   
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = {  :host => 'http://3.114.51.178' }
+  config.action_mailer.default_url_options = {  :host => '3.114.51.178' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:"smtp.gmail.com",
