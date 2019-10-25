f000b475891ac5b39a65a0cd4792301f57923c5c
zackieee <zackie.jackie.222@gmail.com>
2019-10-25 21:46:39 +0900

    dns設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = {  :host => '3.114.51.178' }
+  config.action_mailer.default_url_options = {  :host => 'http://3.114.51.178' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:"smtp.gmail.com",
@@ -75,7 +75,7 @@ Rails.application.configure do
     user_name: Rails.application.credentials.gmail[:user_name],
     password: Rails.application.credentials.gmail[:password],
     authentication: :plain,
-    enable_starttls_auto: false
+    enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
