eeef0f675402e0ea8787f427e4ffcee36d951b59
zackieee <zackie.jackie.222@gmail.com>
2019-10-25 22:10:58 +0900

    DNS設定戻し

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server 'zackie-jackieeee.net.', user: 'ec2-user', roles: %w{app db web}
+server '3.114.51.178', user: 'ec2-user', roles: %w{app db web}
